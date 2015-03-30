c680400a2ef5620959855712816c7c79a205a82b
Brayan Andres Restrepo Galeano <brayan.restrepo@unillanos.edu.co>
2015-03-30 15:35:41 -0500

    Resulve la Matriz Beta Falta pruebas

--- Optimizacion/src/optimizacion/Optimizacion.java
+++ Optimizacion/src/optimizacion/Optimizacion.java
@@ -25,13 +25,17 @@ public class Optimizacion {
      */
     public Optimizacion(){
         this.posicionBetada = new ArrayList<Integer>();
-        this.zMax = false;
+        this.zMax = true;
+//        double a[][] = {
+//            {1,8,-6,17,-6,0,0,0,0},
+//            {0,3,8,6,4,1,0,0,300},
+//            {0,2,0,7,4,0,1,0,500},
+//            {0,3,8,0,1,0,0,1,200}
+//        };
         double a[][] = {
-            {1,8,-6,17,-6,0,0,0,0},
-            {0,3,8,6,4,1,0,0,300},
-            {0,2,0,7,4,0,1,0,500},
-            {0,3,8,0,1,0,0,1,200}
-        };
+            {1,-10,-6,0},
+            {0,6,4,360},
+            {0,4,6,480}};
         this.matriz = a;
     }
     
@@ -52,23 +56,27 @@ public class Optimizacion {
     public void iteraciones(){
         int x = 0;
         int y = 0;
+        
         while(x>=0){
+            System.out.println("X -> "+x);
+            System.out.println("Y -> "+y);
+            this.imprimirMatriz();
             if (zMax) {
                 x = this.masNegativo(this.matriz[0]);
-                if(x==-1){
+                if(x > 0){
+                    y = this.eligirFila(x,this.matriz[0].length-1);
+                }else{
                     break;
                 }
-                y = this.eligirFila(x,this.matriz[0].length-1);
-                System.out.println(y);
             }else{
                 x = this.masPositivo(this.matriz[0]);
-                if(x==-1){
+                if(x > 0){
+                    y = this.eligirFila(x,this.matriz[0].length-1);
+                }else{
                     break;
-                }
-                y = this.eligirFila(x,this.matriz[0].length-1);
-                System.out.println(y);
+                }                    
             }
-            
+            this.operarMatriz(x, y);
         }
     }
     
@@ -89,7 +97,7 @@ public class Optimizacion {
             }
             i++;
         }
-        System.out.println(p);
+        
         return p;
     }
     
@@ -111,13 +119,14 @@ public class Optimizacion {
             }
             i++;
         }
-        System.out.println(p);
+        
         return p;
     }
     
     /**
      * Se divide c2 en c1 (c2/c1) y se octiene el menor numero.
      * RESTRICCOINES:
+     *      si C2 = Negativo no de toma
      *      Si c2 = 0 solo se escoge una vez
      *      Si c1 es Negativo (-) no se divide no se toma esa posicion
      *      si c1 = 0 no coger 
@@ -128,30 +137,58 @@ public class Optimizacion {
     public int eligirFila(int x,int y){
         int p = -1;
         double menorPositivo = Double.MAX_VALUE;
+        
         for (int i = 1; i < this.matriz.length; i++) {
             System.out.println("-> "+this.matriz[i][x]+" -> "+this.matriz[i][y]);
             double aux = this.matriz[i][y]/this.matriz[i][x];
             
             if (this.matriz[i][x] <= 0) {
                 continue;                
-            }else if(menorPositivo >= aux){
+            }else if(menorPositivo >= aux && aux >= 0){
                 //Si c2 es 0 beta esa pocicion y no entra otravez
                 if (this.matriz[i][y] == 0 && !this.posicionBetada.contains(i)) {
                     this.posicionBetada.add(i);
                     menorPositivo = aux;
                     p = i;
-                    System.out.println("s");
                 }else if(this.matriz[i][y] != 0){
                     menorPositivo = aux;
                     p = i;
                 }
             }
         }
-        
         return p;
     }
     
     
+    public void operarMatriz(int x,int y){
+        
+        boolean controlador = true;
+        double pivote = this.matriz[y][x];
+        for (int i = y; i < this.matriz.length; i++) {//Filas 
+            for (int j = 0; j < this.matriz[0].length; j++) {//Columnas
+                if(controlador){
+                    this.matriz[i][j] = this.matriz[y][j]/pivote;
+                }else if(i != y){
+                    this.matriz[i][j] = this.matriz[i][j] - this.matriz[y][j]*this.matriz[i][j];
+                }
+            }
+            if (controlador) {
+                controlador = false;
+                i = -1;
+            }
+        }
+    }
+    
+    
+    public void imprimirMatriz(){
+        for (int i = 0; i < this.matriz.length; i++) {
+            System.out.println("");
+            for (int j = 0; j < this.matriz[0].length; j++) {
+                System.out.print(" "+this.matriz[i][j]);
+            }
+        }
+        System.out.println("\n");
+    }
     public static void main(String[] args) {
         new Optimizacion().iteraciones();
     }
